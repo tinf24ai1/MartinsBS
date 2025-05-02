d4d066c0d56c7bb17014d26dd905c7c79bae989c
juliandieskau <traumaam@gmail.com>
2025-05-02 15:11:36 +0200

    Added permissions and receiver for system boot up and method to start app after intent is received.

---
+++ app/src/main/java/com/firsty/bildtest/core/receivers/BootUpReceiver.kt
@@ -0,0 +1,16 @@
+package com.firsty.bildtest.core.receivers
+
+import com.firsty.bildtest.MainActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootUpReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val openAppIntent = Intent(context, MainActivity::class.java)
+            openAppIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(openAppIntent)
+        }
+    }
+}
