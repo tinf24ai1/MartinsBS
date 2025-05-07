adf167a8ea87d4d942156d680109547a57336dfa
Noah <noah-schwuerzinger@web.de>
2025-05-07 23:00:41 +0200

    importing reorderable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("sh.calvin.reorderable:reorderable:2.4.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
